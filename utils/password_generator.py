9a196b96cf0b98016e08e105eb2a872e376db2fb
username <super.d3280@ya.ru>
2021-12-21 08:18:05 +0300

    feat: added django, grafana, prometheus

---
+++ utils/password_generator.py
@@ -0,0 +1,7 @@
+"""This script is needed to generate password hash for prometheus."""
+import getpass
+import bcrypt
+
+password = getpass.getpass("password: ")
+hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+print(hashed_password.decode())
